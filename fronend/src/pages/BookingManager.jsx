26e79781e024ad27240df0443b967b61d594fd71
nk2811 <khangle1102@gmail.com>
2023-11-17 14:02:21 +0700

    update booking page

--- fronend/src/pages/BookingManager.jsx
+++ fronend/src/pages/BookingManager.jsx
@@ -18,5 +18,54 @@ export default function BookingManager() {
     return `${day}-${month}-${year}`;
   }
 
-  
+  return (
+    <div className="">
+      {bookings.length === 0 && (
+        <h1 className="text-center text-3xl font-semibold my-20">
+          You don&apos;t have any booking !!!
+        </h1>
+      )}
+      {bookings.length !== 0 && (
+        <div className="flex place-content-center mt-8">
+          <table className="py-10 w-3/4">
+            <thead>
+              <tr>
+                <th className="border-b border-gray-300 py-2 w-64">Hotel</th>
+                <th className="border-b border-gray-300 py-2 w-1/4">Contact</th>
+                <th className="border-b border-gray-300 py-2">Phone</th>
+                <th className="border-b border-gray-300 py-2">Check-in</th>
+                <th className="border-b border-gray-300 py-2">Check-out</th>
+                <th className="border-b border-gray-300 py-2">Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bookings?.length > 0 &&
+                bookings.map((booking) => (
+                  <tr key={booking._id}>
+                    <td className="border-b border-gray-300 py-2 text-center px-2">
+                      {booking.place.title}
+                    </td>
+                    <td className="border-b border-gray-300 py-2 text-center text-clip overflow-hidden px-2">
+                      {booking.user.email}
+                    </td>
+                    <td className="border-b border-gray-300 py-2 text-center  px-2">
+                      {booking.phone}
+                    </td>
+                    <td className="border-b border-gray-300 py-2 text-center px-2">
+                      {formatDate(booking.checkIn)}
+                    </td>
+                    <td className="border-b border-gray-300 py-2 text-center px-2">
+                      {formatDate(booking.checkOut)}
+                    </td>
+                    <td className="border-b border-gray-300 py-2 text-center px-2">
+                      {booking.price}
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </div>
+  );
 }
--- fronend/src/pages/BookingPage.jsx
+++ fronend/src/pages/BookingPage.jsx
@@ -1,6 +1,9 @@
 import { useParams, Navigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import BookingDates from "../BookingDates";
+import Rate from "../Rate";
+
 
 export default function BookingPage() {
   const { id } = useParams();
@@ -35,5 +38,41 @@ export default function BookingPage() {
     setRedirect(true);
   }
 
+  return (
+    <div className="my-4 lg:mx-80 mx-10">
+      <h1 className="text-3xl">{booking.place.title}</h1>
+      <div className="bg-gray-200 p-6 my-6 rounded-2xl flex items-center justify-between">
+        <div>
+          <h2 className="text-2xl mb-4">Your booking information:</h2>
+          <BookingDates booking={booking} />
+        </div>
+        <div className="bg-primary p-6 text-white rounded-2xl">
+          <div>Total price</div>
+          <div className="text-3xl">${booking.price}</div>
+        </div>
+      </div>
 
+      <button
+        onClick={deleteBooking}
+        className=" mt-8 w-full bg-red-400 text-white mb-4 py-2 border rounded-xl text-xl font-semibold"
+      >
+        Cancel booking
+      </button>
+      <div className="pt-5 border-t-2 mt-3">
+        <label className="block mb-2 text-xl font-semibold">Your review</label>
+        <textarea
+          value={comment}
+          onChange={(ev) => setComment(ev.target.value)}
+          id="message"
+          rows="4"
+          className="block p-2.5 w-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+          placeholder="Write your thoughts here..."
+        ></textarea>
+        <h1 className="font-semibold text-xl py-3">
+          How do you rate our hotel?
+        </h1>
+        <Rate rating={rate} onRating={(rate) => setRate(rate)} />
+      </div>
+    </div>
+  );
 }
